16f2bbb20732139ec0441f4f86545e815dbec1f5
段佳良 <duanjialiang@xd.com>
2023-02-15 15:12:04 +0800

    init: fcp

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -33,7 +33,7 @@ setTimeout(async () => {
         debug: true,
         // Alternatively, use `process.env.npm_package_version` for a dynamic release version
         // if your build tool supports it.
-        release: process.env.npm_package_version || projectVersion,
+        release: projectVersion,
         integrations: [new BrowserTracing()],
 
         // Set tracesSampleRate to 1.0 to capture 100%
